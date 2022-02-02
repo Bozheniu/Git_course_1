032bd99943ebeac210be886a0284f4ed8c1ca454
Bozheniu <bozheniu@gmail.com>
2022-02-02 14:00:29 +0100

    Домашка урок 4

---
+++ Zherebilo_Bozhena_task_4/task_4_4.py
@@ -0,0 +1,11 @@
+import utils
+
+print(utils.currency_rates("eur"))
+print(utils.currency_rates("PLN"))
+print(utils.currency_rates("USD"))
+
+'''(venv) bozeniu@MacBook-Air-Bozena Zherebilo_Bozhena_task_4 % python task_4_4.py
+86.2826
+18.9615
+76.4849
+'''
